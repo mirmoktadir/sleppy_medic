0afb763e89a64c142eb6962cfdc758a7b1632b8e
mirmoktadir <mirmoktadir@gmail.com>
2023-02-09 19:00:58 +0600

    home page

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:sleepy_medic/utils/constants.dart';
+import '../../../../config/theme/my_fonts.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -10,11 +13,153 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('All Posts'),
-        centerTitle: true,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100.h),
+        child: AppBar(
+          toolbarHeight: 120.h,
+          backgroundColor: theme.scaffoldBackgroundColor,
+          title: Image.asset(
+            AppImages.kLogo,
+            height: 100.h,
+            width: 100.w,
+          ),
+          leading: const Icon(Icons.menu, size: 35),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: Icon(Icons.people, size: 35),
+            ),
+          ],
+          centerTitle: true,
+        ),
       ),
-      body: SizedBox(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome back,Mir!",
+                style: theme.textTheme.displaySmall,
+              ),
+              Text(
+                "How are you feeling today ?",
+                style: TextStyle(
+                  fontSize: MyFonts.headline6TextSize,
+                  color: theme.hintColor,
+                ),
+              ),
+              SizedBox(height: 30.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        height: 70.h,
+                        width: 70.w,
+                        padding: EdgeInsets.all(15.r),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18.r),
+                          color: Colors.white,
+                        ),
+                        child: Image.asset(
+                          AppImages.kCalm,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text("Calm"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 70.h,
+                        width: 70.w,
+                        padding: EdgeInsets.all(15.r),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18.r),
+                          color: Colors.white,
+                        ),
+                        child: Image.asset(
+                          AppImages.kRelax,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text("Relax"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 70.h,
+                        width: 70.w,
+                        padding: EdgeInsets.all(15.r),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18.r),
+                          color: Colors.white,
+                        ),
+                        child: Image.asset(
+                          AppImages.kFocus,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text("Focus"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 70.h,
+                        width: 70.w,
+                        padding: EdgeInsets.all(15.r),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18.r),
+                          color: Colors.white,
+                        ),
+                        child: Image.asset(
+                          AppImages.kRelax,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text("Anxious"),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 30.h),
+              Container(
+                height: 180.h,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.r),
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 30.h),
+              Container(
+                height: 180.h,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.r),
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 30.h),
+              Container(
+                height: 180.h,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.r),
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,4 +2,8 @@ class AppImages {
   static String get kAppIcon => 'assets/images/app_icon.png';
   static String get kBackground => 'assets/images/Background.png';
   static String get kLogo => 'assets/images/Logo.png';
+  static String get kRelax => 'assets/images/Relax.png';
+  static String get kAnx => 'assets/images/Anxious.png';
+  static String get kCalm => 'assets/images/Calm - Icon.png';
+  static String get kFocus => 'assets/images/Focus.png';
 }
